5190ee730b2c007c6f8fd7ecdf5092eba1a82fbf
UmmeySalma <77966775+UmmeySalma@users.noreply.github.com>
2021-04-05 16:55:35 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,19 @@
+class Slingshot {
+    constructor(bodyA,bodyB) {
+      var options = {
+          length:10,
+          stifness:0.04,
+          bodyA:bodyA,
+          bodyB:bodyB,
+
+      }
+      this.slingshot = Constraint.create(options);
+      World.add(world, this.slingshot);
+    }
+    display(){
+        var pointA = this.slingshot.bodyA.position;
+        var pointB = this.slingshot.bodyB.position
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+  };
